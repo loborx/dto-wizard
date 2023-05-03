e3f175d33882eb8d8721358f054089fec7cf9415
Piotr Skrobol <pio.skro@gmail.com>
2023-05-03 21:55:33 +0200

    Add readonly id property and magic getters/setters to Post class

--- examples/Post.php
+++ examples/Post.php
@@ -5,9 +5,33 @@ declare(strict_types=1);
 namespace Loborx\DtoWizardExamples;
 
 use Loborx\DtoWizard\DataObject;
+use ReflectionClass;
 
+/**
+ * @property string $id
+ * @property string $title
+ * @property string $body
+ */
 class Post extends DataObject
 {
-    public string $title;
-    public string $body;
+    private readonly string $id;
+    private string $title;
+    private string $body;
+
+    public function __set(string $name, mixed $value): void
+    {
+        $reflection = new ReflectionClass($this);
+        $property = $reflection->getProperty($name);
+
+        if ($property->isInitialized($this) && $property->isReadOnly()) {
+            return;;
+        }
+
+        $this->{$name} = $value;
+    }
+
+    public function __get(string $name): mixed
+    {
+        return $this->{$name};
+    }
 }
