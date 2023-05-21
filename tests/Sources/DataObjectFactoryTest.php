968628eabecb3b12473865474c94dc26ff4c2a68
Piotr Skrobol <pio.skro@gmail.com>
2023-05-21 23:40:53 +0200

    WIP

--- tests/Sources/DataObjectFactoryTest.php
+++ tests/Sources/DataObjectFactoryTest.php
@@ -5,18 +5,9 @@ declare(strict_types=1);
 namespace Loborx\DtoWizardTests\Sources;
 
 use Loborx\DtoWizard\DataObjectFactory;
-use Loborx\DtoWizardTests\TestCase;
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\Test;
 
-#[CoversClass(DataObjectFactory::class)]
-class DataObjectFactoryTest extends TestCase
-{
-    #[Test]
-    public function it_is_abstract(): void
-    {
-        $reflection = new \ReflectionClass(DataObjectFactory::class);
+it('is abstract class', function () {
+    $reflection = new \ReflectionClass(DataObjectFactory::class);
 
-        $this->assertTrue($reflection->isAbstract());
-    }
-}
+    expect($reflection->isAbstract())->toBeTrue();
+});
