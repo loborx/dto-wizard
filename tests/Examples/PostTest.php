943f3f4b58307ed5a7a4240c2a6383f3097b7113
Piotr Skrobol <pio.skro@gmail.com>
2023-05-21 19:42:15 +0200

    Add Pest Faker plugin for PostTest

--- tests/Examples/PostTest.php
+++ tests/Examples/PostTest.php
@@ -5,88 +5,60 @@ declare(strict_types=1);
 namespace Loborx\DtoWizardTests\Examples;
 
 use Loborx\DtoWizard\DataObject;
-use Loborx\DtoWizard\Traits\WithFaker;
 use Loborx\DtoWizardExamples\Post;
-use Loborx\DtoWizardTests\TestCase;
-use PHPUnit\Framework\Attributes\CoversClass;
-use PHPUnit\Framework\Attributes\Test;
+use function Pest\Faker\fake;
 
-#[CoversClass(Post::class)]
-class PostTest extends TestCase
-{
-    use WithFaker;
+it('is instantiable', function () {
+    $this->assertInstanceOf(Post::class, new Post());
+});
 
-    #[Test]
-    public function it_is_instantiable(): void
-    {
-        $this->assertInstanceOf(Post::class, new Post());
-    }
+it('has a title', function () {
+    $post = new Post();
+    $title = fake()->sentence();
+    $post->title = $title;
 
-    #[Test]
-    public function a_post_has_a_title(): void
-    {
-        $post = new Post();
-        $title = $this->faker->sentence();
+    $this->assertEquals($title, $post->title);
+});
 
-        $post->title = $title;
+it('has a body', function () {
+    $body = fake()->paragraph();
+    $post = new Post();
+    $post->body = $body;
 
-        $this->assertEquals($title, $post->title);
-    }
+    $this->assertEquals($body, $post->body);
+});
 
-    #[Test]
-    public function a_post_has_a_body(): void
-    {
-        $body = $this->faker->paragraph();
+it('has attributes created by constructor', function () {
+    $rawData = [
+        'title' => fake()->sentence(),
+        'body' => fake()->paragraph()
+    ];
 
-        $post = new Post();
-        $post->body = $body;
+    $post = new Post($rawData);
 
-        $this->assertEquals($body, $post->body);
-    }
+    $this->assertEquals($post->title, $rawData['title']);
+    $this->assertEquals($post->body, $rawData['body']);
+});
 
-    #[Test]
-    public function a_post_attributes_can_be_created_by_constructor(): void
-    {
-        $rawData = [
-            'title' => $this->faker->sentence(),
-            'body' => $this->faker->paragraph()
-        ];
+it('extends data object', function () {
+    $this->assertInstanceOf(DataObject::class, new Post());
+});
 
-        $post = new Post($rawData);
+it('has a settable title', function () {
+    $post = new Post(['title' => fake()->sentence]);
+    $post->title = $newTitle = fake()->sentence;
 
-        $this->assertEquals($post->title, $rawData['title']);
-        $this->assertEquals($post->body, $rawData['body']);
-    }
+    $this->assertEquals($post->title, $newTitle);
+});
 
-    #[Test]
-    public function it_extends_data_object(): void
-    {
-        $post = new Post();
+it('has non settable id', function () {
+    $rawData = [
+        'id' => $id = fake()->uuid()
+    ];
 
-        $this->assertInstanceOf(DataObject::class, $post);
-    }
+    $post = new Post($rawData);
 
-    #[Test]
-    public function a_post_has_settable_title(): void
-    {
-        $post = new Post(['title' => $this->faker->sentence]);
-        $post->title = $newTitle = $this->faker->sentence;
+    $post->set('id', fake()->uuid());
 
-        $this->assertEquals($post->title, $newTitle);
-    }
-
-    #[Test]
-    public function a_post_has_non_settable_id(): void
-    {
-        $this->markTestIncomplete();
-        $rawData = [
-            'id' => $id = $this->faker->uuid()
-        ];
-
-        $post = new Post($rawData);
-
-            $post->set('id', $this->faker->uuid());
-
-        $this->assertEquals($id, $post->id);
-    }
-}
+    $this->assertEquals($id, $post->id);
+})->todo();
